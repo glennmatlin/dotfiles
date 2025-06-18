0ae9b507023105d530dd7001c4ee45a56af6e061
Glenn Matlin <glennmatlin@users.noreply.github.com>
2025-06-17 22:29:56 -0400

    add erdtree and broot to wsl bootstrapping

--- wsl_bootstrap.sh
+++ wsl_bootstrap.sh
@@ -18,3 +18,8 @@ sudo dpkg -i cuda-repo-wsl-ubuntu-11-8-local_11.8.0-1_amd64.deb
 sudo cp /var/cuda-repo-wsl-ubuntu-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
 sudo apt-get -y install cuda
+
+
+brew install erdtree
+brew install broot
+brew install neovim
